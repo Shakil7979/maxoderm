7807576c2ad9177c04da90054fdd60ba4d179d78
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-09-21 02:59:53 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,23 @@ jQuery(function($) {
         $(this).next().slideToggle();
     });
 });
+ 
+
+
+$(document).ready(function(){
+    $(".experiance_carousel").owlCarousel({
+        loop:true, 
+        nav:true,
+        dots:false,
+        navText : ["<i class='fa-solid fa-arrow-left-long'></i>","<i class='fa-solid fa-arrow-right-long'></i>"],
+        items:1 
+    });
+    $(".client_reviews").owlCarousel({
+        loop:true, 
+        nav:false,
+        dots:false, 
+        margin: 20,
+        items:3 
+    });
+});
+  
