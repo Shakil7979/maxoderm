312bf0bb322512e3e2450ac3f265470d35219315
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-09-25 00:31:21 +0600

    update

---
+++ maxoderm2/assets/js/scripts.js
@@ -0,0 +1,41 @@
+jQuery(function($) {
+    $('.toggle_title').click(function() {
+        $(this).toggleClass('selected');
+        $(this).next().slideToggle();
+    });
+});
+ 
+
+
+$(document).ready(function(){
+    $(".experiance_level").owlCarousel({
+        loop:true, 
+        nav:true,
+        dots:false,
+        navText : ["<i class='fa-solid fa-arrow-left-long'></i>","<i class='fa-solid fa-arrow-right-long'></i>"],
+        items:1 
+    });
+    $(".client_reviews").owlCarousel({
+        loop:true, 
+        nav:false,
+        dots:false, 
+        margin: 20,
+        items:3,
+        responsive : { 
+            0 : { 
+                items:1,
+            }, 
+            576 : { 
+                items:1,
+            }, 
+            768 : { 
+                items:3,
+            }
+        }
+    });
+
+    $(document).on('click','.mobile_bars', function(){
+        $('.mobile_menu').slideToggle();
+    })
+});
+  
